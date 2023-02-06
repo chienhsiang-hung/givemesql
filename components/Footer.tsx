2e7c056f6ce2961130d8a733141c9c650fc61f94
chienhsiang-hung <77676044+chienhsiang-hung@users.noreply.github.com>
2023-02-06 21:40:07 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -37,7 +37,7 @@ export default function Footer() {
           </svg>
         </Link>
         <Link
-          href="https://github.com/Nutlope/twitterbio"
+          href="https://github.com/chienhsiang-hung/givemesql"
           className="group"
           aria-label="TaxPal on GitHub"
         >
