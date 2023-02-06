a7aeca246fbaf100c52ce4972188ac37fa29fc1e
chienhsiang-hung <77676044+chienhsiang-hung@users.noreply.github.com>
2023-02-06 23:10:46 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ module.exports = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://chienhsiang-hung.github.io/" },
+          { key: "Access-Control-Allow-Origin", value: "chienhsiang-hung.github.io" },
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ]
