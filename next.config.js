e302fd6af16c44abec4693c70fc2193618968f18
chienhsiang-hung <77676044+chienhsiang-hung@users.noreply.github.com>
2023-02-06 22:00:17 +0800

    revert

--- next.config.js
+++ next.config.js
@@ -1,22 +1,4 @@
-// next.config.js
-
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
+/** @type {import('next').NextConfig} */
 module.exports = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  images: {
-    loader: 'imgix',
-    path: 'the "domain" of your Imigix source',
-  },
+  reactStrictMode: true,
 }
