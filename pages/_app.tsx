f92ffab0324dd43f192e88278489c1a6b4b0a6de
chienhsiang-hung <77676044+chienhsiang-hung@users.noreply.github.com>
2023-02-06 23:39:31 +0800

    GoogleAnalytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <GoogleAnalytics trackPageViews gaMeasurementId="G-EB1W4TXEV1" />
       <Component {...pageProps} />
       <Analytics />
     </>
