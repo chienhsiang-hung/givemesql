266a8683e1e42fa1e7d0e44b6a0c89f5e04ab2ad
chienhsiang-hung <77676044+chienhsiang-hung@users.noreply.github.com>
2023-02-06 22:04:50 +0800

    cors

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+
+  async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'https://givemesql.vercel.app/:path*',
+        },
+      ]
+    },
+
 }
