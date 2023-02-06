8b95bacd9b3ab25f927d6ff02b76379c98bfe86e
chienhsiang-hung <77676044+chienhsiang-hung@users.noreply.github.com>
2023-02-06 21:51:52 +0800

    deploy test

---
+++ next.config.js
@@ -0,0 +1,22 @@
+// next.config.js
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    loader: 'imgix',
+    path: 'the "domain" of your Imigix source',
+  },
+}
